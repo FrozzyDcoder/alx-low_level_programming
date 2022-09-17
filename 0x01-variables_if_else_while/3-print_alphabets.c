4a64e87fe08f22be6ebec4c7a6685a940e77b947
Fortune Umoh <fortuneumoh10@gmail.com>
2022-09-17 08:24:12 -0700

    printing

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main -main block
+ * Description: print alphabets is lower then upper case
+ * return:0
+ */
+int main(void)
+{
+	char c = 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	c = 'A';
+
+		while (c <= 'Z')
+		{
+			putchar(c);
+			c++;
+		}
+
+	putchar("\n");
+
+return (0);
+}
