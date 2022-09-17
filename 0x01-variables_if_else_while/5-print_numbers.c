0adf8bb405cb0410b7f5534b1b317325ce8370d8
Fortune Umoh <fortuneumoh10@gmail.com>
2022-09-17 15:57:16 -0700

    oswalt

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,13 +3,14 @@
 #include <time.h>
 /**
  * main - main block
- * Description:  a program that prints all single digit numbers of base 10 starting from 0, followed by a new line.
+ * Description:  a program that prints all single numbers of base 10 starting from 0.
  * Return: 0
  */
 int main(void)
 {
 	char c = '0';
-	while ( c <= '9' )
+	while (c <= '9')
+
 	{
 		putchar(c);
 		c++;
