fed508e63e66bf61df699b352888d1378f093eab
Fortune Umoh <fortuneumoh10@gmail.com>
2022-09-18 01:32:44 -0700

    poster

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: printing all numbers of base 16 in lower case.
+ * Return: 0
+ */
+int main(void)
+{
+	char x;
+	int y;
+
+	for (y = 0; y < 10; y++)
+	{
+		putchar(y + '0');
+	}
+
+	for (x = 'a'; x < 'g'; x++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
