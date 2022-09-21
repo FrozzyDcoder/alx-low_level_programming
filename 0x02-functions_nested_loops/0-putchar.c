c8cca5fa5042b37436d08bb7a2802f5d370aca27
Fortune Umoh <fortuneumoh10@gmail.com>
2022-09-21 14:35:40 -0700

    nest

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <string.h>
 
-/** 
+/**
  * main - Entry point
  * Description: prints _putchar using putchar prototype
  * Return: Always 0 (Success)
@@ -16,5 +16,5 @@ int main(void)
 		_putchar(str[ch]);
 	_putchar('\n');
 
-	return(0);
+	return (0);
 }
